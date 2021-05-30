46390310cca271f139c85087ea19d8a8f1b86924
Charith Prabhagya <charith.prabhagya@cinglevue.com>
2021-05-30 14:08:43 +0530

    inline template,style and data binging string interpolation

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./app.component.css']
+  // styles: [
+  //   `
+  //     h2 {
+  //       color: dodgerblue;
+  //     }
+  //   `
+  // ]
 })
-export class AppComponent {
-}
+export class AppComponent {}
