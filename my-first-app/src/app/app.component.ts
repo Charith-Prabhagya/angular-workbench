eb28f54bbee5e763047242cc9de1398224c5a9f2
Charith Prabhagya <charith.prabhagya@cinglevue.com>
2021-06-05 12:46:46 +0530

    Outputting Lists with ngFor, Indexing

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,17 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>
-  // `,
   styleUrls: ['./app.component.css']
-  // styles: [
-  //   `
-  //     h2 {
-  //       color: dodgerblue;
-  //     }
-  //   `
-  // ]
 })
-export class AppComponent {}
+export class AppComponent {
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+}
